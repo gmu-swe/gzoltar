d1f290dae87ff11cdb057357feb7c0ad53209ab6
Jonathan Bell <jon@jonbell.net>
2019-06-28 13:20:37 -0400

    Brute force solution for all frame issues related to NEW instructions

--- com.gzoltar.core/src/main/java/com/gzoltar/core/instr/analysis/CoverageAnalyser.java
+++ com.gzoltar.core/src/main/java/com/gzoltar/core/instr/analysis/CoverageAnalyser.java
@@ -15,10 +15,15 @@
  */
 package com.gzoltar.core.instr.analysis;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.commons.AnalyzerAdapter;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.FrameNode;
 import org.objectweb.asm.tree.LabelNode;
 import org.objectweb.asm.tree.MethodNode;
 import com.gzoltar.core.instr.CoverageClassVisitor;
@@ -61,6 +66,11 @@ public class CoverageAnalyser extends MethodNode {
 
   @Override
   public void visitEnd() {
+
+    //Later on we will run into a mess if we insert a jump between a label and a NEW that follows it.
+    //As a countermeasure, we defensively add new labels for each NEW and rewrite all frames.
+    fixLabelsForFrames();
+
     final List<Block> blocks = findRequriedProbeLocations();
 
     final int blockCount = blocks.size();
@@ -83,6 +93,41 @@ public class CoverageAnalyser extends MethodNode {
               analyzerAdapter, this.addFrames, this.access, this.name, this.desc, this.probeGroup, skipExceptionProbes),
               counter));
 
+  }
+
+  private void fixLabelsForFrames() {
+    AbstractInsnNode insn = this.instructions.getFirst();
+    LabelNode lastLabel = null;
+    HashMap<LabelNode, LabelNode> labelToLabel = new HashMap<>();
+    LinkedList<FrameNode> frames = new LinkedList<>();
+    while(insn != null){
+      if(insn instanceof LabelNode)
+        lastLabel = (LabelNode) insn;
+      else if(insn instanceof FrameNode)
+        frames.add((FrameNode) insn);
+      else if(insn.getOpcode() == Opcodes.NEW){
+        //Add a new label before the NEW
+        LabelNode newLbl = new LabelNode(new Label());
+        this.instructions.insertBefore(insn, newLbl);
+        if(lastLabel != null)
+          labelToLabel.put(lastLabel, newLbl);
+      }
+      insn = insn.getNext();
+    }
+
+    for(FrameNode fn : frames){
+      for(int i = 0; i < fn.local.size(); i++){
+      	if(fn.local.get(i) != null && labelToLabel.containsKey(fn.local.get(i))){
+      	  fn.local.set(i, labelToLabel.get(fn.local.get(i)));
+        }
+      }
+      for(int i = 0; i < fn.stack.size(); i++){
+      	Object o = fn.stack.get(i);
+      	if(fn.stack.get(i) != null && labelToLabel.containsKey(fn.stack.get(i))){
+      	  fn.stack.set(i, labelToLabel.get(fn.stack.get(i)));
+        }
+      }
+    }
   }
 
   private List<Block> findRequriedProbeLocations() {
